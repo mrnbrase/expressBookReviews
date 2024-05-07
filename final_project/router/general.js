35fd22569edd851cbba68cc8c7b634761a59da07
mrnbrase <mrnbrase@hotmail.com>
2024-05-06 20:27:17 -0400

    update files

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,8 +1,8 @@
 const express = require('express');
-let books = require("./booksdb.js");
-let users = require("./auth_users.js").users;
-const public_users = express.Router();
+const books = require("./booksdb.js");
+const users = require("./auth_users.js").users;
 const jwt = require('jsonwebtoken');
+const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
     const { username, password } = req.body;
@@ -80,6 +80,55 @@ public_users.delete("/review/:isbn", (req, res) => {
             res.status(404).json({ message: "Review not found" });
         }
     });
+});
+
+// Get the book list available in the shop
+public_users.get('/', function (req, res) {
+  res.json(books);
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+  const { isbn } = req.params;
+  const book = books[isbn];
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
+});
+
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+  const { author } = req.params;
+  const booksByAuthor = Object.values(books).filter(book => book.author === author);
+  if (booksByAuthor.length > 0) {
+    res.json(booksByAuthor);
+  } else {
+    res.status(404).json({ message: "Books by this author not found" });
+  }
+});
+
+// Get all books based on title
+public_users.get('/title/:title', function (req, res) {
+  const { title } = req.params;
+  const booksWithTitle = Object.values(books).filter(book => book.title === title);
+  if (booksWithTitle.length > 0) {
+    res.json(booksWithTitle);
+  } else {
+    res.status(404).json({ message: "Books with this title not found" });
+  }
+});
+
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  const { isbn } = req.params;
+  const book = books[isbn];
+  if (book && book.reviews) {
+    res.json(book.reviews);
+  } else {
+    res.status(404).json({ message: "Reviews for this book not found" });
+  }
 });
 
 module.exports.general = public_users;
